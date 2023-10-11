d19d34077e165e47454c707fe094215a32be2d89
Myo Thant <jamesrobinson636478@gmail.com>
2023-10-12 00:13:00 +0630

    add getUser route

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,10 @@
 const authRoute = require('./router');
 const { isAuthenticated } = require('../middleware/auth');
 
-const { loginUser,logoutUser } = require('../controllers/authController');
+const { loginUser,logoutUser,getUser } = require('../controllers/authController');
 
 authRoute.route('/login').post(loginUser);
 authRoute.route('/logout').get(isAuthenticated, logoutUser);
+authRoute.route('/me').get(isAuthenticated, getUser);
 
 module.exports = authRoute;
