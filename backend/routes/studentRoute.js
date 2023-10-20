3f3b4d98ff3a58a39da7a2ae7e37d12481902821
Myo Thant <jamesrobinson636478@gmail.com>
2023-10-20 22:54:40 +0630

    Change StudentRoute permission to admin only

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -3,9 +3,9 @@ const { isAuthenticated, authorizeRoles } = require('../middleware/auth');
 
 const { getStudents, newStudents, updateStudents, deleteStudents } = require('../controllers/studentControllers');
 
-userRoute.route('/admin/students').get(isAuthenticated, authorizeRoles('admin','user'), getStudents)
+userRoute.route('/admin/students').get(isAuthenticated, authorizeRoles('admin'), getStudents)
 .delete(isAuthenticated, authorizeRoles('admin'), deleteStudents);
 userRoute.route('/admin/student/new').post(isAuthenticated, authorizeRoles('admin'), newStudents);
-userRoute.route('/admin/student/update').put(isAuthenticated, authorizeRoles('admin','user'), updateStudents);
+userRoute.route('/admin/student/update').put(isAuthenticated, authorizeRoles('admin'), updateStudents);
 
 module.exports = userRoute;
