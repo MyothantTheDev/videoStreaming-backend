3fb8687a94f3fc7235f423c4f89274f4794dd2a8
Myo Thant <jamesrobinson636478@gmail.com>
2023-10-29 09:50:16 +0630

    add student delete route

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -3,8 +3,8 @@ const { isAuthenticated, authorizeRoles } = require('../middleware/auth');
 
 const { getStudents, newStudents, updateStudents, deleteStudents } = require('../controllers/studentControllers');
 
-userRoute.route('/admin/students').get(isAuthenticated, authorizeRoles('admin'), getStudents)
-.delete(isAuthenticated, authorizeRoles('admin'), deleteStudents);
+userRoute.route('/admin/students').get(isAuthenticated, authorizeRoles('admin'), getStudents);
+userRoute.route('/admin/students').delete(isAuthenticated, authorizeRoles('admin'), deleteStudents);
 userRoute.route('/admin/student/new').post(isAuthenticated, authorizeRoles('admin'), newStudents);
 userRoute.route('/admin/student/update').put(isAuthenticated, authorizeRoles('admin'), updateStudents);
 
