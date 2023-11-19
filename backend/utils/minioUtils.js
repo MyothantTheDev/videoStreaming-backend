c41c85a234f4b9512f116b3b796eb9acdc142512
Myo Thant <jamesrobinson636478@gmail.com>
2023-11-19 20:28:12 +0630

    fix minio bucket api

--- backend/utils/minioUtils.js
+++ backend/utils/minioUtils.js
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose');
-const Video = require('../models/video');
 const Batch = require('../models/batch');
 const minioClient = require('../config/minioclient');
 
 async function minioObjectDelete(object) {
-    let backet = await Batch.findById(object.batchId);
+    let backet = await Batch.findById(object.batchId._id);
     await minioClient.removeObject(backet.name, object.title);
 }
 
