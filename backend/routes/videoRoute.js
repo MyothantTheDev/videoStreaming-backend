bf2af9f25bec4d1d3bdb82d3451d9d8706a43646
Myo Thant <jamesrobinson636478@gmail.com>
2023-11-19 20:24:44 +0630

    Add video stream route

--- backend/routes/videoRoute.js
+++ backend/routes/videoRoute.js
@@ -1,5 +1,5 @@
 const videoRouter = require('./router');
-const { newVideo, getVideo, updateVideo, deleteVideo } = require('../controllers/videoController');
+const { newVideo, getVideo, updateVideo, deleteVideo, streamVideo, combineVideo } = require('../controllers/videoController');
 const { isAuthenticated, authorizeRoles } = require('../middleware/auth');
 const upload = require('../middleware/storage');
 
@@ -7,5 +7,7 @@ videoRouter.route('/video/new').post(isAuthenticated, authorizeRoles('admin'), u
 videoRouter.route('/video').get(isAuthenticated, authorizeRoles('admin', 'user'),getVideo);
 videoRouter.route('/video/delete').delete(isAuthenticated, authorizeRoles('admin'), deleteVideo);
 videoRouter.route('/video/update').put(isAuthenticated, authorizeRoles('admin'), upload.single('file'),updateVideo);
+videoRouter.route('/video/stream').get(isAuthenticated, authorizeRoles('admin','user'), streamVideo);
+videoRouter.route('/video/combine').post(isAuthenticated, authorizeRoles('admin'), combineVideo);
 
 module.exports = videoRouter;
