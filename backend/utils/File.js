6417315628fd9ee8639fd72e3415b5397dc7391b
Myo Thant <jamesrobinson636478@gmail.com>
2023-11-19 20:25:58 +0630

    Add file assemble service

--- backend/utils/File.js
+++ backend/utils/File.js
@@ -1,18 +1,42 @@
 const fs = require('fs');
 const path = require('path');
+const minioClient = require('../config/minioclient');
 
 const pathDir = path.join(path.dirname(__dirname),'upload');
 
-exports.createPath = () => {
-    fs.mkdirSync(pathDir, { recursive: true}, err => {
+exports.createPath = (dirname) => {
+    const targetFolder = path.join(pathDir, dirname);
+    fs.mkdirSync(targetFolder, { recursive: true}, err => {
         if(err) console.log(err);
     })
 }
 
-exports.isdir = () => {
-    return fs.existsSync(pathDir);
+exports.isdir = (dirname) => {
+    const dir = path.join(pathDir, dirname);
+    return fs.existsSync(dir);
 }
 
-exports.getFolder = () => {
-    return pathDir;
+exports.getFolder = (dirname) => {
+    return path.join(pathDir, dirname);
+}
+
+exports.assembleAndSaveFile = async (fileId, totalChunks, title) => {
+    const assembleFilePath = path.join(pathDir, fileId, title);
+    const chunkBuffers = [];
+
+    for (let index = 0; index < totalChunks; index++) {
+        const chunkPath = path.join(pathDir, fileId, `${index}`);
+        const chunkContent = fs.readFileSync(chunkPath);
+        chunkBuffers.push(chunkContent);
+    }
+
+    const assembleContent = Buffer.concat(chunkBuffers);
+    fs.writeFileSync(assembleFilePath, assembleContent);
+
+    for (let index = 0; index < totalChunks; index++) {
+        const chunkPath = path.join(pathDir, fileId, `${index}`);
+        fs.unlinkSync(chunkPath);
+    }
+
+    return assembleFilePath;
 }
