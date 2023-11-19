c21081d4965977b550cae6325f54d1e6b5fa415b
Myo Thant <jamesrobinson636478@gmail.com>
2023-11-19 20:20:26 +0630

    Change to Memory Storage

--- backend/config/storage.js
+++ backend/config/storage.js
@@ -1,20 +1,4 @@
 const multer = require('multer');
-const path = require('path');
-const Batch = require('../models/batch');
-const Video = require('../models/video');
-const { isdir,createPath,getFolder } = require('../utils/File');
 
-const Storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        const path = getFolder();
-        if(!isdir()) {
-            createPath();
-        }
-        cb(null, path);
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname);
-    }
-})
-
+const Storage = multer.memoryStorage();
 module.exports = Storage;
